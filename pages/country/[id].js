829be8f829ea305424dc766a5c63fa2f26cb6441
Ali Fadda <silvertechguy@gmail.com>
2020-11-11 20:56:50 +0200

    getServerSideProps (Server Side Rendering) and getStaticProps/getStaticPaths

--- pages/country/[id].js
+++ pages/country/[id].js
@@ -1,3 +1,35 @@
-const Country = () => <div>Country</div>;
+const getCountry = async (id) => {
+  const res = await fetch(`https://restcountries.eu/rest/v2/alpha/${id}`);
+  const country = await res.json();
+  return country;
+};
+
+const Country = ({ country }) => {
+  return <div>{country.name}</div>;
+};
 
 export default Country;
+
+export const getStaticPaths = async () => {
+  const res = await fetch("https://restcountries.eu/rest/v2/all");
+  const countries = await res.json();
+
+  const paths = countries.map((country) => ({
+    params: { id: country.alpha3Code },
+  }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps = async ({ params }) => {
+  const country = await getCountry(params.id);
+
+  return {
+    props: {
+      country,
+    },
+  };
+};
