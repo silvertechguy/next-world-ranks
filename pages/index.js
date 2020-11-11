bf115a047d7888fce3d657d3b8e868b74066a647
Ali Fadda <silvertechguy@gmail.com>
2020-11-11 16:38:13 +0200

    Build Layout Component

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,5 @@
-import Head from "next/head";
-import styles from "../styles/Home.module.css";
+import Layout from "../components/Layout/Layout";
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>main</main>
-
-      <footer className={styles.footer}>footer</footer>
-    </div>
-  );
+  return <Layout />;
 }
